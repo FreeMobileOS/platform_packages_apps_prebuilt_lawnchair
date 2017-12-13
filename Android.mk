b774c1a44d7ddb552e699618748e78f01fb0c6d5
Satish Patel <tosatishpatel@gmail.com>
2017-12-13 14:34:13 +0530

    Add lawnfeed
    lawnfeed is used to check and update newer version for lawnchair
    
    Without this, lawnchair keep showing "update pop up" on boot
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,10 +1,28 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+############################################
+# Lawnchair 
+############################################
 LOCAL_MODULE := Lawnchair
 LOCAL_PACKAGE_NAME := Lawnchair
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher3 Launcher2
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_PATH = $(TARGET_OUT)/priv-app
+include $(BUILD_PREBUILT)
+
+############################################
+# lawnfeed
+############################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := Lawnfeed
+LOCAL_PACKAGE_NAME := Lawnfeed
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
